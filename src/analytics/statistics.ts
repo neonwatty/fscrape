c2e0b356c49949327033d2fba347d9ae67b0473a
Jeremy Watt <jeremywatt@Jeremys-Mac-mini.local>
2025-09-13 18:39:06 -0700

    Completed Task 18.2.2: Trend Detection Engine

--- src/analytics/statistics.ts
+++ src/analytics/statistics.ts
@@ -436,20 +436,21 @@ export class StatisticsEngine {
 
   private static lnGamma(x: number): number {
     const cof = [
+      // eslint-disable-next-line no-loss-of-precision
       76.18009172947146, -86.50532032941677, 24.01409824083091,
-      -1.231739572450155, 0.1208650973866179e-2, -0.5395239384953e-5,
+      -1.231739572450155, 0.001208650973866179, -0.000005395239384953,
     ];
 
     let y = x;
     let tmp = x + 5.5;
     tmp -= (x + 0.5) * Math.log(tmp);
 
-    let ser = 1.000000000190015;
+    let ser = 1.00000000019;
     for (let j = 0; j < 6; j++) {
       ser += cof[j] / ++y;
     }
 
-    return -tmp + Math.log((2.5066282746310005 * ser) / x);
+    return -tmp + Math.log((2.506628274631 * ser) / x);
   }
 
   static calculateZScore(value: number, mean: number, stdDev: number): number {
